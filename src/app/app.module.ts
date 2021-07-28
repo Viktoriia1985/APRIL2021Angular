47cc091821576939a42170a146661b45f23256f2
Viktoriia Krat <flightsspb@gmail.com>
2021-07-28 17:08:39 +0300

    lesson 3 resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule, Routes} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {UserComponent, UserDetailsComponent, UsersComponent} from './components';
+import { UserGuardService } from './services';
+
+
+let routes: Routes = [
+  {
+    path: '',
+    component: UsersComponent,
+    canDeactivate: [UserGuardService]
+  },
+  {
+    path: 'users/:id',
+    component: UserDetailsComponent,
+    canActivate: [UserGuardService],
+    canDeactivate: [UserGuardService]
+  },
+  {
+    path: 'users',
+    redirectTo: '',
+    pathMatch: 'full'},
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    UserDetailsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
